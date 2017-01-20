ffb84c729f1c762cd5e5a9f8456ea7417781146d
Kaz Nishimura <kazssym@vx68k.org>
2017-01-20 20:33:57 +0900

    Add a new Java file

---
+++ editor/src/main/java/org/vx68k/webapp/editor/AccessControlFilter.java
@@ -0,0 +1,56 @@
+/*
+ * AccessControlFilter
+ * Copyright (C) 2017 Kaz Nishimura
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.vx68k.webapp.editor;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ *
+ * @author Kaz Nishimura
+ */
+@WebFilter(urlPatterns = {"/*"})
+public class AccessControlFilter implements Filter {
+
+  private FilterConfig config;
+
+  @Override
+  public void init(FilterConfig config) throws ServletException
+  {
+    this.config = config;
+  }
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response,
+      FilterChain chain) throws ServletException, IOException
+  {
+    chain.doFilter(request, response);
+  }
+
+  @Override
+  public void destroy()
+  {
+  }
+}
