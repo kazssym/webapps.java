8a93df9b42d54802069a94e864a4d77d999fbd1b
Kaz Nishimura <kazssym@vx68k.org>
2018-05-05 10:45:25 +0900

    Use flex layout

--- editor/src/main/webapp/resources/scripts/layout.js
+++ editor/src/main/webapp/resources/scripts/layout.js
@@ -6,25 +6,3 @@
   permitted in any medium without royalty provided the copyright notice and
   this notice are preserved.  This file is offered as-is, without any warranty.
 */
-
-(function () {
-  "use strict";
-
-  var fill = function () {
-    var htmlRect = document.documentElement.getBoundingClientRect();
-    var height = htmlRect.bottom - htmlRect.top;
-
-    var top = document.getElementById("top");
-    var topRect = top.getBoundingClientRect();
-    height -= topRect.bottom - topRect.top;
-
-    var content = document.getElementById("content");
-    content.style.height = height + "px";
-  };
-
-  window.addEventListener("resize", function (/*event*/) {
-    fill();
-  });
-
-  fill();
-})();
