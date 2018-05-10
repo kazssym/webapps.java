c367bdb5299ef0e82bd6d04c516b50ae92fa1314
Kaz Nishimura <kazssym@vx68k.org>
2018-05-10 13:34:29 +0900

    Enable CodeMirror

--- editor/src/main/webapp/resources/scripts/editor.js
+++ editor/src/main/webapp/resources/scripts/editor.js
@@ -1,1 +1,19 @@
 // editor.js - editor control script
+// Copyright (C) 2018 Kaz Nishimura
+//
+// Copying and distribution of this file, with or without modification, are
+// permitted in any medium without royalty provided the copyright notice and
+// this notice are preserved.  This file is offered as-is, without any warranty.
+
+"use strict";
+
+// CodeMirror instance.
+let editor;
+
+document.addEventListener("DOMContentLoaded", () => {
+        let textArea = document.getElementById("editor-text");
+        if (textArea != null) {
+            editor = CodeMirror.fromTextArea(textArea, {
+                });
+        }
+    });
