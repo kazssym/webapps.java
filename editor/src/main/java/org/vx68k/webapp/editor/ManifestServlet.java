e2f32ef2be79b5b9ff9a74fc50ca27cd6a6a0914
Kaz Nishimura <kazssym@vx68k.org>
2018-05-14 12:58:51 +0900

    Add a new class

---
+++ editor/src/main/java/org/vx68k/webapp/editor/ManifestServlet.java
@@ -0,0 +1,60 @@
+/*
+ * ManifestServlet.java - application manifest generator
+ * Copyright (C) 2018 Kaz Nishimura
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package org.vx68k.webapp.editor;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Application manifest generator.
+ * @author Kaz Nishimura
+ */
+@WebServlet(value = {"/manifest/*"})
+public final class ManifestServlet extends HttpServlet
+{
+    private static final long serialVersionUID = 1L;
+
+    private static final String CONTENT_TYPE = "application/manifest+json";
+
+    /**
+     * Generates an application manifest.
+     *
+     * @param request {@inheritDoc}
+     * @param response {@inheritDoc}
+     * @exception ServletException {@inheritDoc}
+     * @exception IOException {@inheritDoc}
+     */
+    @Override
+    protected void doGet(final HttpServletRequest request,
+                         final HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        response.setContentType(CONTENT_TYPE);
+        // TODO: Construct a real manifest.
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().print("{}");
+        response.getWriter().close();
+    }
+}
