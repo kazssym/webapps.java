767b99bc213b09febb22f404414b24b8d1e4c973
Kaz Nishimura <kazssym@vx68k.org>
2018-05-24 18:54:33 +0900

    Delete an unused import.

--- editor/src/test/java/org/vx68k/webapp/editor/tests/ManifestTest.java
+++ editor/src/test/java/org/vx68k/webapp/editor/tests/ManifestTest.java
@@ -26,8 +26,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
-import javax.json.JsonValue;
-
 import org.junit.Test;
 import org.vx68k.webapp.editor.Manifest;
 import org.vx68k.webapp.editor.ManifestImage;
