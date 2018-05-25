54c88ef520a8d2c5bdb02ff01ff3b85317bee485
Kaz Nishimura <kazssym@vx68k.org>
2018-05-26 00:24:08 +0900

    Add new classes

---
+++ manifest/src/main/java/org/vx68k/webapp/manifest/Manifest.java
@@ -0,0 +1,144 @@
+/*
+ * Manifest.java - web app manifest
+ * Copyright (C) 2018 Kaz Nishimura
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package org.vx68k.webapp.manifest;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+
+/**
+ * Web app manifest.
+ *
+ * @author Kaz Nishimura
+ * @since 1.0
+ * @see <a href="https://www.w3.org/TR/appmanifest/">Web App Manifest</a>
+ */
+public class Manifest implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Name of the web app.
+     */
+    private String name;
+
+    /**
+     * Short name of the web app.
+     */
+    private String shortName;
+
+    /**
+     * Icons of the web app.
+     */
+    private ManifestImage[] icons;
+
+    /**
+     * Returns the name of the web app.
+     *
+     * @return the name, or {@code null} if not specified
+     */
+    public final String getName()
+    {
+        return name;
+    }
+
+    /**
+     * Sets the name of the web app.
+     *
+     * @param value the new name
+     */
+    public final void setName(final String value)
+    {
+        name = value;
+    }
+
+    /**
+     * Returns the short name of the web app.
+     *
+     * @return the short name, or {@code null} if not specified
+     */
+    public final String getShortName()
+    {
+        return shortName;
+    }
+
+    /**
+     * Sets the short name of the web app.
+     *
+     * @param value the new short name
+     */
+    public final void setShortName(final String value)
+    {
+        shortName = value;
+    }
+
+    /**
+     * Returns the icons of the web app.
+     *
+     * @return the icons, or {@code null} if not specified
+     */
+    public final ManifestImage[] getIcons()
+    {
+        return icons;
+    }
+
+    /**
+     * Sets the icons of the web app.
+     *
+     * @param value the new icons
+     */
+    public final void setIcons(final ManifestImage[] value)
+    {
+        icons = value;
+    }
+
+    /**
+     * Returns a JSON object that represents this manifest.
+     *
+     * @return a JSON object
+     */
+    public final JsonObject toJsonObject()
+    {
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+        if (name != null) {
+            builder.add("name", name);
+        }
+        if (shortName != null) {
+            builder.add("short_name", shortName);
+        }
+        if (icons != null) {
+            JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+            Arrays.stream(icons).forEachOrdered((icon) -> {
+                    if (icon != null) {
+                        arrayBuilder.add(icon.toJsonObject());
+                    }
+                    else {
+                        arrayBuilder.addNull();
+                    }
+                });
+            builder.add("icons", arrayBuilder);
+        }
+        return builder.build();
+    }
+}
---
+++ manifest/src/test/java/org/vx68k/webapp/manifest/tests/ManifestTest.java
@@ -0,0 +1,111 @@
+/*
+ * ManifestTest.java
+ * Copyright (C) 2018 Kaz Nishimura
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package org.vx68k.webapp.manifest.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.vx68k.webapp.manifest.Manifest;
+import org.vx68k.webapp.manifest.ManifestImage;
+
+/**
+ * Test fixture for {@link Manifest}.
+ *
+ * @author Kaz Nishimura
+ * @since 1.0
+ */
+public final class ManifestTest
+{
+    /**
+     * Tests the {@code name} property.
+     */
+    @Test
+    public void testName()
+    {
+        Manifest manifest = new Manifest();
+        assertNull("manifest.name", manifest.getName());
+        assertFalse("'name' in JSON(manifest)",
+            manifest.toJsonObject().containsKey("name"));
+
+        manifest.setName("a name");
+        assertEquals("manifest.name", "a name", manifest.getName());
+        assertEquals("JSON(manifest)['name']", "a name",
+            manifest.toJsonObject().getString("name"));
+
+        manifest.setName(null);
+        assertNull("manifest.name", manifest.getName());
+    }
+
+    /**
+     * Tests the {@code shortName} property.
+     */
+    @Test
+    public void testShortName()
+    {
+        Manifest manifest = new Manifest();
+        assertNull("manifest.shortName", manifest.getShortName());
+        assertFalse("'short_name' in JSON(manifest)",
+            manifest.toJsonObject().containsKey("short_name"));
+
+        manifest.setShortName("a short name");
+        assertEquals("manifest.shortName", "a short name",
+            manifest.getShortName());
+        assertEquals("JSON(manifest)['short_name']", "a short name",
+            manifest.toJsonObject().getString("short_name"));
+
+        manifest.setShortName(null);
+        assertNull("manifest.shortName", manifest.getShortName());
+    }
+
+    /**
+     * Tests the {@code icons} property.
+     */
+    @Test
+    public void testIcons()
+    {
+        Manifest manifest = new Manifest();
+        assertNull("manifest.icons", manifest.getIcons());
+        assertFalse("'icons' in JSON(manifest)",
+            manifest.toJsonObject().containsKey("icons"));
+
+        manifest.setIcons(new ManifestImage[0]);
+        assertEquals("manifest.icons.length", 0, manifest.getIcons().length);
+        assertEquals("JSON(manifest)['icons'].length", 0,
+            manifest.toJsonObject().getJsonArray("icons").size());
+
+        manifest.setIcons(new ManifestImage[1]);
+        assertNull("manifest.icons[0]", manifest.getIcons()[0]);
+        assertTrue("JSON(manifest)['icons'][0]",
+            manifest.toJsonObject().getJsonArray("icons").isNull(0));
+
+        manifest.setIcons(new ManifestImage[] {new ManifestImage()});
+        assertNotNull("manifest.icons[0]", manifest.getIcons()[0]);
+        assertNotNull("JSON(manifest)['icons'][0]",
+            manifest.toJsonObject().getJsonArray("icons").getJsonObject(0));
+
+        manifest.setIcons(null);
+        assertNull("manifest.icons", manifest.getIcons());
+    }
+}
