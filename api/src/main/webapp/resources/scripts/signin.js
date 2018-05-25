56d385be90ceed63cd97a025a49c186b9c134d85
Kaz Nishimura <kazssym@vx68k.org>
2018-05-25 22:47:23 +0900

    Add permission notice

--- api/src/main/webapp/resources/scripts/signin.js
+++ api/src/main/webapp/resources/scripts/signin.js
@@ -1,4 +1,9 @@
+// signin.js - sign-in control script
+// Copyright (C) 2018 Kaz Nishimura
 //
+// Copying and distribution of this file, with or without modification, are
+// permitted in any medium without royalty provided the copyright notice and
+// this notice are preserved.  This file is offered as-is, without any warranty.
 
 "use strict";
 
