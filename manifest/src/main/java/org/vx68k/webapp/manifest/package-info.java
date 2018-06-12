4531beec6b8e292a9aede05e2429c6824891bf3e
Kaz Nishimura <kazssym@vx68k.org>
2018-06-12 21:37:59 +0900

    Modify comments

--- manifest/src/main/java/org/vx68k/webapp/manifest/package-info.java
+++ manifest/src/main/java/org/vx68k/webapp/manifest/package-info.java
@@ -1,5 +1,5 @@
 /*
- * package-info.java
+ * package-info.java - package org.vx68k.webapp.manifest
  * Copyright (C) 2018 Kaz Nishimura
  *
  * This program is free software: you can redistribute it and/or modify it
