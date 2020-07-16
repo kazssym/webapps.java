fb056ad1201f233f868757803e51c84eb3bc6353
Kaz Nishimura <kazssym@linuxfront.com>
2020-07-16 19:13:02 +0900

    Register the service worker

--- toybox/src/main/webapp/resources/ws/init.js
+++ toybox/src/main/webapp/resources/ws/init.js
@@ -4,3 +4,13 @@
 // Copying and distribution of this file, with or without modification, are
 // permitted in any medium without royalty provided the copyright notice and
 // this notice are preserved.  This file is offered as-is, without any warranty.
+
+// This file is a module script and shall be in strict mode by default.
+
+navigator.serviceWorker.register("../service.js")
+    .then((registration) => {
+        console.debug("registered service worker: %o", registration);
+    })
+    .catch((reason) => {
+        console.log(reason);
+    });
