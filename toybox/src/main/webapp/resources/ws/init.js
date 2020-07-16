c0936636f068484867e327ce41174706f60bba97
Kaz Nishimura <kazssym@linuxfront.com>
2020-07-16 19:35:16 +0900

    Get the context path from the script URL

--- toybox/src/main/webapp/resources/ws/init.js
+++ toybox/src/main/webapp/resources/ws/init.js
@@ -7,7 +7,10 @@
 
 // This file is a module script and shall be in strict mode by default.
 
-navigator.serviceWorker.register("../service.js")
+let url = new URL(import.meta.url);
+let context = url.hash.substring(1);
+
+navigator.serviceWorker.register(`${context}/service.js`)
     .then((registration) => {
         console.debug("registered service worker: %o", registration);
     })
