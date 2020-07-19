28807d726503916d370f184535dc6b83ce7dbe97
Kaz Nishimura <kazssym@linuxfront.com>
2020-07-19 21:27:30 +0900

    Add new class 'ControlMessage' with its decoder and encoder

---
+++ server-agent/src/main/java/org/vx68k/webapp/server/ControlMessageEncoder.java
@@ -0,0 +1,55 @@
+/*
+ * ControlMessageEncoder.java
+ * Copyright (C) 2020 Kaz Nishimura
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package org.vx68k.webapp.server;
+
+import java.nio.ByteBuffer;
+import javax.websocket.Encoder;
+import javax.websocket.EndpointConfig;
+
+public class ControlMessageEncoder implements Encoder.Binary<ControlMessage>
+{
+    private EndpointConfig endpointConfig;
+
+    public final EndpointConfig getEndpointConfig()
+    {
+        return endpointConfig;
+    }
+
+    @Override
+    public final ByteBuffer encode(final ControlMessage message)
+    {
+        ByteBuffer buffer = ByteBuffer.allocate(2);
+        buffer.put(0, (byte)0x80);
+        return buffer;
+    }
+
+    @Override
+    public final void init(final EndpointConfig endpointConfig)
+    {
+        this.endpointConfig = endpointConfig;
+    }
+
+    @Override
+    public final void destroy()
+    {
+        endpointConfig = null;
+    }
+}
